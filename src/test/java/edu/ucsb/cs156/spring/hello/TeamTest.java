4f57c17c5029e194b66732fc4f44d18234d36a73
Jayden Tan <jaydentan@ucsb.edu>
2025-10-07 21:19:54 -0700

    added team .equals testing

--- src/test/java/edu/ucsb/cs156/spring/hello/TeamTest.java
+++ src/test/java/edu/ucsb/cs156/spring/hello/TeamTest.java
@@ -1,6 +1,7 @@
 package edu.ucsb.cs156.spring.hello;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
+import java.util.ArrayList;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -23,4 +24,54 @@ public class TeamTest {
     // TODO: Add additional tests as needed to get to 100% jacoco line coverage, and
     // 100% mutation coverage (all mutants timed out or killed)
 
+    @Test
+    public void toString_returns_correct_string() {
+        assertEquals("Team(name=test-team, members=[])", team.toString());
+    }
+
+    @Test
+    public void equals_sameObject_returns_true(){
+        assertTrue(team.equals(team));
+    }
+
+    @Test
+    public void equals_null_returns_false(){
+        assertFalse(team.equals(null));
+    }
+
+    @Test
+    public void equals_diffObject_returns_false(){
+        assertFalse(team.equals("team"));
+    }
+
+    @Test
+    public void equals_sameName_and_sameMembers_returns_true(){
+        
+        Team other = new Team("test-team");
+        assertTrue(team.equals(other));
+    }
+
+    @Test
+    public void equals_sameName_and_diffMembers_returns_false(){
+        ArrayList<String> otherMembers = new ArrayList<>();
+        otherMembers.add("Bobby");
+        Team other = new Team("test-team");
+        other.setMembers(otherMembers);
+        assertFalse(team.equals(other));
+    }
+
+    @Test
+    public void equals_diffName_and_sameMembers_returns_false(){
+        Team other = new Team("test-team1");
+        assertFalse(team.equals(other));
+    }
+
+    @Test
+    public void equals_diffName_and_diffMembers_returns_false(){
+        ArrayList<String> otherMembers = new ArrayList<>();
+        otherMembers.add("Bobby");
+        Team other = new Team("test-team1");
+        other.setMembers(otherMembers);
+        assertFalse(team.equals(other));
+    }
 }
